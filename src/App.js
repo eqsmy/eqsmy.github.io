c828f0874ce8d2c4a4668d25110cf0f3edeab590
eqsmy <elysiaqs@gmail.com>
2021-08-14 16:39:26 -0700

    first draft complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import PageRouter from "./components/route/route.js";
-import useHashLocation from "./services/wouter-hash";
-
-import "./styles/styles.css";
+import "./styles.css";
 
 function App() {
   return (
-    <Router hook={useHashLocation}>
+    <div>
       <header className="header">
         <div className="links">
           <a href="/#">&nbsp;Home&nbsp;</a>
@@ -21,15 +16,117 @@ function App() {
         </div>
       </header>
       <main role="main" className="wrapper">
-        <div className="content">
-          {/* Router specifies which component to insert here as the main content */}
-          <Home />
-          <section id="resume">
-            <About />
+        <div className="page content">
+          <div className="full home">
+            <h1 align="center">Hello!</h1>
+            <space />
+            <h2 align="center">I'm Elysia Smyers</h2>
+            <div align="center">
+              <img
+                src="https://cdn.glitch.com/5fe60a68-2fd8-4c91-b74a-b259c25c948a%2FIMG_1164_Original.jpg?v=1627958979432"
+                className="illustration"
+                alt="Photo of Elysia"
+              />
+            </div>
+            <div align="center" className="spaced">
+              <a href="#aboutme" className="active">
+                <img
+                  className="arrow"
+                  src="https://cdn.glitch.com/5fe60a68-2fd8-4c91-b74a-b259c25c948a%2FGroup%201%20(1).png?v=1628033858326"
+                />
+              </a>
+            </div>
+          </div>
+
+          <section id="aboutme" className="aboutme full">
+            <box className="narrow">
+              <p className="in-box">A bit about me</p>
+            </box>
+            <box className="narrow">
+              <p className="in-box">
+                I am a rising Junior at Stanford University majoring in{" "}
+                <mark>&nbsp;Symbolic Systems&nbsp;</mark> with a concentration
+                in Human-Centered Artificial Intelligence. I want to{" "}
+                <u>
+                  develop products that can make a real difference in people's
+                  lives
+                </u>
+                . I strongly believe that nothing is truly impossible to
+                accomplish given the right team of people and the willingness to
+                get your hands dirty and listen to every relevant feedback loop
+                available.
+              </p>
+            </box>
+
+            <box className="narrow">
+              <p className="in-box">
+                <em>
+                  My current experience revolves predominantly around Product
+                  Management and development of internal tools, and I am very
+                  interested in stepping into more-technical roles to further
+                  hone my <mark>&nbsp;software engineering&nbsp;</mark> skills.
+                  Check out my{" "}
+                  <mark>
+                    &nbsp;
+                    <u>
+                      <a href="#resume">Resume</a>
+                    </u>
+                    &nbsp;
+                  </mark>{" "}
+                  for more information about my work and leadership experience.
+                </em>
+              </p>
+            </box>
+            <div align="center" className="spaced">
+              <a href="#resume" className="active">
+                &nbsp;
+                <img
+                  className="arrow"
+                  src="https://cdn.glitch.com/5fe60a68-2fd8-4c91-b74a-b259c25c948a%2FGroup%201%20(1).png?v=1628033858326"
+                />
+              </a>
+              &nbsp;
+            </div>
+          </section>
+
+          <section className="full resume" id="resume">
+            <box className="narrow">
+              <p className="in-box">My Resume</p>
+            </box>
+            <box className="narrow">
+              <p className="in-box">
+                See my resume below to learn about my recent work experiences
+                and interests. Please feel free to reach out to me about
+                anything on my resume --{" "}
+                <mark>
+                  &nbsp;I love meeting and connecting with new people!&nbsp;
+                </mark>
+                My contact information is available below on my resume, or you
+                can click the tab above to access my LinkedIn profile.
+              </p>
+              <p className="in-box">
+                <a
+                  target="_blank"
+                  href="https://docs.google.com/document/d/e/2PACX-1vQeQ5pM6nWZiGXzMD5AMa_UkqNi0Qtq7rVV7i8OqbrYBZySqGC9MihKHoCDJGUVRsiWXpngYxCBtrQ4/pub"
+                >
+                  <u>
+                    (Click here to view resume if it is not appearing below.)
+                  </u>
+                </a>
+              </p>
+            </box>
+            <p align="center">
+              <iframe
+                id="test"
+                className="c1"
+                frameBorder="0"
+                src="https://docs.google.com/document/d/e/2PACX-1vQeQ5pM6nWZiGXzMD5AMa_UkqNi0Qtq7rVV7i8OqbrYBZySqGC9MihKHoCDJGUVRsiWXpngYxCBtrQ4/pub?embedded=true"
+              ></iframe>
+            </p>
           </section>
         </div>
       </main>
-    </Router>
+    </div>
   );
 }
 
